d3f1d9fe9a385441a9bdb2ab2c0f0b35cffcac97
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-04-10 02:31:56 -0300

    remove observability

--- src/modules/get_user/app/get_user_presenter.py
+++ src/modules/get_user/app/get_user_presenter.py
@@ -5,25 +5,25 @@ from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHt
 from src.shared.infra.external.observability.observability_aws import ObservabilityAWS
 
 
-observability = Environments.get_observability()(module_name="get_user")
+# observability = Environments.get_observability()(module_name="get_user")
 
 repo = Environments.get_user_repo()()
-usecase = GetUserUsecase(repo, observability=observability)
-controller = GetUserController(usecase, observability=observability)
+usecase = GetUserUsecase(repo)
+controller = GetUserController(usecase)
 
-@observability.presenter_decorators
+# @observability.presenter_decorators
 def get_user_presenter(event):
     httpRequest = LambdaHttpRequest(data=event)
     response = controller(httpRequest)
     httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
     return httpResponse.toDict()
 
-@observability.handler_decorators
+# @observability.handler_decorators
 def lambda_handler(event, context):
     
     response = get_user_presenter(event)
     
-    observability.add_metric(name="ErrorCount", unit="Count", value=1) if response["statusCode"] != 200 else None # ErrorCount metrics
+    # observability.add_metric(name="ErrorCount", unit="Count", value=1) if response["statusCode"] != 200 else None # ErrorCount metrics
     
     return response
 

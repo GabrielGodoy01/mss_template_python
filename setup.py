5c9bed8b34944fe65bebaecb13edd63f9ac7a574
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-06-30 23:48:57 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+def read_requirements():
+    with open('requirements.txt') as req_file:
+        return req_file.read().splitlines()
+
 setup(
     name='core',
-    version='0.1',
-    packages=find_packages(),
-    include_package_data=True,
+    version='0.1.0',
+    package_dir={'core': '.'},
+    packages=['core'],
+    install_requires=read_requirements(),
 )

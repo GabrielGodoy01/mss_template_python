b18de4974b7f86aafcf567c348f990d2e966d432
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-06-30 23:53:37 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='core',
     version='0.1',
+    package_dir={'shared': 'shared'},
     packages=['shared'],
     include_package_data=True,
+    install_requires=[
+        # Lista de dependências do core application, ou
+        # você pode ler de requirements.txt se preferir
+        # 'requests', 'flask', etc.
+    ],
 )

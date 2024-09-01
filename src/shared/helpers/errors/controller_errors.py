4ed9dd9708897a1743b5d95ee83043b5509ea5d6
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-09-01 12:28:25 -0300

    att template

--- src/shared/helpers/errors/controller_errors.py
+++ src/shared/helpers/errors/controller_errors.py
@@ -3,7 +3,7 @@ from src.shared.helpers.errors.base_error import BaseError
 
 class MissingParameters(BaseError):
     def __init__(self, message: str):
-        super().__init__(f'Field {message} is missing')
+        super().__init__(f'Campo {message} está faltando')
 class WrongTypeParameter(BaseError):
     def __init__(self, fieldName: str, fieldTypeExpected: str, fieldTypeReceived: str):
-        super().__init__(f'Field {fieldName} isn\'t in the right type.\n Received: {fieldTypeReceived}.\n Expected: {fieldTypeExpected}')
+        super().__init__(f"O campo '{fieldName}' deveria ser do tipo '{fieldTypeExpected}', mas foi recebido '{fieldTypeReceived}'")
